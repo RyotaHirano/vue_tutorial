04b951beb586d2f90f25595f5f39e872d957579f
RyotaHirano <hirano@tsumikiinc.com>
2016-12-28 15:53:45 +0900

    [fix] ListView Components

--- src/js/components/ListView.js
+++ src/js/components/ListView.js
@@ -7,7 +7,7 @@ Vue.component('list-view', {
   },
   template: `
     <div class="list-view">
-      <div v-if="memos && memos.lenght !== 0">
+      <div v-if="memos && memos.length !== 0">
         <list-item v-for="memo in memos" :memo="memo"></list-item>
       </div>
       <div v-else>
